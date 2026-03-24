21d486d53cf3f54809900d27105a6b2e896843cb
Challa Sanjan Shivadeep Reddy <90202905+SanjanShiv@users.noreply.github.com>
2026-03-25 04:15:23 +0530

    Update api.js

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://e-commerce-0ubl.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
